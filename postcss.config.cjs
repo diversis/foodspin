0fe7f563d054f16219869b0f2caf58d6892310be
Noxyangel <forgotrrr@gmail.com>
2023-11-11 23:47:00 +0300

    style: :ambulance: add btn classes to purgecss safelist

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,23 +1,23 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-	plugins: [
-		require("autoprefixer"),
+    plugins: [
+        require("autoprefixer"),
 
-		// require("@fullhuman/postcss-purgecss")({
-		// 	content: ["*.html"],
-		// 	extractors: [
-		// 		{
-		// 			extractor: (content) =>
-		// 				content.match(/[\w-/:]+(?<!:)/g) ||
-		// 				[],
-		// 			extensions: ["html"],
-		// 		},
-		// 	],
-		// 	whitelist: ["html", "body", "hidden"],
-		// }),
-		require("postcss-normalize"),
-		require("cssnano")({ preset: "default" }),
-	],
+        require("@fullhuman/postcss-purgecss")({
+            content: ["*.html"],
+            extractors: [
+                {
+                    extractor: (content) =>
+                        content.match(/[\w-/:]+(?<!:)/g) ||
+                        [],
+                    extensions: ["html"],
+                },
+            ],
+            whitelist: ["html", "body", "hidden"],
+        }),
+        require("postcss-normalize"),
+        require("cssnano")({ preset: "default" }),
+    ],
 };
 
 module.exports = config;
