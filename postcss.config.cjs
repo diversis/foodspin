ecb1c0a765dab7b3444bb48a4b6f50cf008eed6e
Noxyangel <forgotrrr@gmail.com>
2023-11-12 13:34:09 +0300

    style: add logo

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,18 +3,18 @@ const config = {
     plugins: [
         require("autoprefixer"),
 
-        require("@fullhuman/postcss-purgecss")({
-            content: ["*.html"],
-            extractors: [
-                {
-                    extractor: (content) =>
-                        content.match(/[\w-/:]+(?<!:)/g) ||
-                        [],
-                    extensions: ["html"],
-                },
-            ],
-            whitelist: ["html", "body", "hidden"],
-        }),
+        // require("@fullhuman/postcss-purgecss")({
+        //     content: ["*.html"],
+        //     extractors: [
+        //         {
+        //             extractor: (content) =>
+        //                 content.match(/[\w-/:]+(?<!:)/g) ||
+        //                 [],
+        //             extensions: ["html"],
+        //         },
+        //     ],
+        //     whitelist: ["html", "body", "hidden"],
+        // }),
         require("postcss-normalize"),
         require("cssnano")({ preset: "default" }),
     ],
